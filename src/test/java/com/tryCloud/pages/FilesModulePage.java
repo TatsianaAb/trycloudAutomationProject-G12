6650e6463733ff7a42d0b933c6d5faba17e617d8
Tatsiana <tatsianavv@gmail.com>
2022-11-08 23:48:25 -0500

    Some updates made

--- src/test/java/com/tryCloud/pages/FilesModulePage.java
+++ src/test/java/com/tryCloud/pages/FilesModulePage.java
@@ -1,5 +1,6 @@
 package com.tryCloud.pages;
 
+import com.tryCloud.utilities.BrowserUtils;
 import com.tryCloud.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,7 +17,22 @@ public class FilesModulePage {
     @FindBy (xpath = "//label[@for='select_all_files']")
     public WebElement selectAllCheckbox;
 
-
     @FindBy (xpath = "//input[@class='selectCheckBox checkbox']")
     public List<WebElement> checkboxes;
+
+    @FindBy (xpath = "(//a[@class='action action-menu permanent'])[1]")
+    public WebElement firstFileActionIcon;
+
+    @FindBy (xpath = "//div[@id='app-navigation']//a")
+    public List<WebElement> dropdownMenu;
+
+   @FindBy (xpath = "//div//span[@class='nametext']")
+   public List<WebElement> favoriteOptions;
+
+   @FindBy (xpath = "//div[@class='fileActionsMenu popovermenu bubble open menu']//li//span[2]")
+   public List<WebElement> actionIconOptions;
+
+
+
+
 }
